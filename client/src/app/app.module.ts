2c3bb00e6c33b922592a7aa24a544bb79389dbec
Sebastien Cadorette <cado.94@me.com>
2018-07-16 21:30:31 -0400

    New component created.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { ButtonMenuComponent } from './features/button-menu/button-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ client/src/app/features/button-menu/button-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-menu',
+  templateUrl: './button-menu.component.html',
+  styleUrls: ['./button-menu.component.css']
+})
+export class ButtonMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
