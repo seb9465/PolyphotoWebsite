0601c04a80c63752283ed122a6924c68cbe128d4
Sebastien Cadorette <cado.94@me.com>
2018-07-16 21:59:51 -0400

    Changed css to scss.

--- client/src/app/features/button-menu/button-menu.component.ts
+++ client/src/app/features/button-menu/button-menu.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button-menu',
-  templateUrl: './button-menu.component.html',
-  styleUrls: ['./button-menu.component.css']
+    selector: 'app-button-menu',
+    templateUrl: './button-menu.component.html',
+    styleUrls: ['./button-menu.component.scss']
 })
 export class ButtonMenuComponent implements OnInit {
+    @Input()
+    textSize: number;
 
-  constructor() { }
+    private text: string;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.text = 'hello';
+    }
+
+    ngOnInit() {
+    }
 
 }
