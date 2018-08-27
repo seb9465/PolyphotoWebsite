e677d76d80ddeaf97d05dde4f7de5071704d71c7
SebastienCadorette <cado.94@me.com>
2018-08-27 09:23:03 -0400

    Added the main reservation component.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { FooterComponent } from './common/footer/footer.component';
+import { MainReservationsComponent } from './reservations/main-reservations/main-reservations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainReservationsComponent
   ],
   imports: [
     BrowserModule,
